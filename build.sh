8a32e399376983eb08389f06e3d5260f1d88dd44
chonlaphoom <chonlaphoomth@gmail.com>
2025-09-25 23:22:45 +0700

    add clean format cls

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ echo "starting building latex"
 set -e
 set -x
 
-/Library/TeX/texbin/pdflatex -halt-on-error -interaction=mode -jobname=chonlaphom-2025 cv.tex
+/Library/TeX/texbin/pdflatex -halt-on-error -jobname=cv cv.tex
 
 echo "finished building latex"
