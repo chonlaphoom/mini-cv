4be31e1c5e5f3dd64f74fe31f71227e3fc4d330d
chonlaphoom <chonlaphoomth@gmail.com>
2025-09-27 02:05:23 +0700

    update font

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ echo "starting building latex"
 set -e
 set -x
 
-/Library/TeX/texbin/pdflatex -halt-on-error -jobname=cv cv.tex
+/Library/TeX/texbin/lualatex -halt-on-error -jobname=cv cv.tex
 
 echo "finished building latex"
